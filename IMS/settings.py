73f3aac90df5836e3ccd06ca17972bb411710160
Eyobel11 <154692239+Eyobel11@users.noreply.github.com>
2024-12-06 23:01:14 +0300

    Update settings.py

--- IMS/settings.py
+++ IMS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@3g!4p%j-$4g3&e#(0gf0#ajlyy8r*q1#0#0j6x@gde!wpk386'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
