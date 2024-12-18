b9baed97b484c9d50092cff1d03914e09e51c351
Eyobel11 <154692239+Eyobel11@users.noreply.github.com>
2024-12-18 20:06:26 +0300

    vercel deploy

--- IMS/settings.py
+++ IMS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@3g!4p%j-$4g3&e#(0gf0#ajlyy8r*q1#0#0j6x@gde!wpk386'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -91,34 +92,34 @@ WSGI_APPLICATION = 'IMS.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'defaultdb',
-#         'USER': 'avnadmin',
-#         'PASSWORD': 'AVNS_wvWFsr284OzCVQmQj-1',
-#         'HOST': 'eyobel-eyobel1.i.aivencloud.com',
-#         'PORT': '20390',
-#         'OPTIONS': {
-#             'sslmode': 'require',
-#         },
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ims_oj1a',
-        'USER': 'ims_oj1a_user',
-        'PASSWORD': 'kohb0ZmrMsjvaKRJ9VJUvIlInXLpQJzi',
-        'HOST': 'dpg-csu78rq3esus73c8faf0-a.oregon-postgres.render.com',
-        'PORT': '5432',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_wvWFsr284OzCVQmQj-1',
+        'HOST': 'eyobel-eyobel1.i.aivencloud.com',
+        'PORT': '20390',
         'OPTIONS': {
             'sslmode': 'require',
         },
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ims_oj1a',
+#         'USER': 'ims_oj1a_user',
+#         'PASSWORD': 'kohb0ZmrMsjvaKRJ9VJUvIlInXLpQJzi',
+#         'HOST': 'dpg-csu78rq3esus73c8faf0-a.oregon-postgres.render.com',
+#         'PORT': '5432',
+#         'OPTIONS': {
+#             'sslmode': 'require',
+#         },
+#     }
+# }
 
 
 # Password validation
@@ -156,6 +157,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR , 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles_build', 'static')
+ 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- warehouse/urls.py
+++ warehouse/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -49,5 +51,6 @@ urlpatterns = [
     path('editstatus/<int:id>/', views.editstatus, name='editstatus'),
 
 
-
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
