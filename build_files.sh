836a9381fe98d2eb10b85dcb0faab18b4a61d1f7
Eyobel11 <154692239+Eyobel11@users.noreply.github.com>
2024-12-18 20:44:46 +0300

    vercel deploy4

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+python3 -m pip install --upgrade pip  # Use the default Python runtime to upgrade pip
+python3 -m pip install -r requirements.txt  # Install dependencies
+python3 manage.py collectstatic --noinput  # Collect static files
+
+# Debugging: Check Python version and pip availability
+python3 --version
+python3 -m pip --version
