89f995ad821f89d7a13e4ef6383adbdd2ba628ba
Eyobel11 <154692239+Eyobel11@users.noreply.github.com>
2024-12-18 21:24:20 +0300

    vercel deploy8

--- IMS/settings.py
+++ IMS/settings.py
@@ -159,6 +159,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
  
 
 # Default primary key field type
